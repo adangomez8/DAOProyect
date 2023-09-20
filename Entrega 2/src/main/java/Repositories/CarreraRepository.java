845afe61dba3d419d75cf2e14617ad307767396a
José Guidi <joseguidi02@gmail.com>
2023-09-20 11:00:42 -0300

    Agrego el codigo restante de los <T>Repository

--- Entrega 2/src/main/java/Repositories/CarreraRepository.java
+++ Entrega 2/src/main/java/Repositories/CarreraRepository.java
@@ -1,33 +1,50 @@
 package Repositories;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 
 import Entities.Carrera;
 
 public class CarreraRepository implements CRUDRepository<Carrera>{
 	
-	private EntityManager em;
+	private EntityManagerFactory emf;
 	
-	public CarreraRepository(EntityManager em) {
-		this.em=em;
+	public CarreraRepository(EntityManagerFactory emf) {
+		this.emf=emf;
 	}
 	
 	public Carrera getById(int id) {
+		EntityManager em = emf.createEntityManager();
 		return em.find(Carrera.class, id);
 	}
 	
 	public void create(Carrera c) {
+		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
 		Carrera car= new Carrera(c.getNombre(),c.getDuracion());
 		em.persist(car);
+		em.getTransaction().commit();
+		em.close();
 	}
 	
 	public void delete(Carrera c) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
 		em.remove(c);
+		em.getTransaction().commit();
+		em.close();
 	}
 
 	@Override
 	public void update(Carrera element) {
-
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		if(em.contains(element)){
+			em.merge(element);
+		}else{
+			// si no esta no quiero agregarlo
+		}
+		em.getTransaction().commit();
+		em.close();
 	}
 }
--- Entrega 2/src/main/java/Repositories/InfoCarreraRepository.java
+++ Entrega 2/src/main/java/Repositories/InfoCarreraRepository.java
@@ -1,33 +1,50 @@
 package Repositories;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 
 import Entities.InfoCarrera;
 
 public class InfoCarreraRepository implements CRUDRepository<InfoCarrera>{
+
+	private EntityManagerFactory emf;
 	
-	private EntityManager em;
-	
-	public InfoCarreraRepository(EntityManager em) {
-		this.em=em;
+	public InfoCarreraRepository(EntityManagerFactory emf) {
+		this.emf=emf;
 	}
 	
 	public InfoCarrera getById(int id) {
+		EntityManager em = emf.createEntityManager();
 		return em.find(InfoCarrera.class, id);
 	}
 	
 	public void create(InfoCarrera inf) {
-		em.getTransaction();
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
 		InfoCarrera i= new InfoCarrera(inf.getCarrera(),inf.isGraduado(),inf.getAntiguedad());
 		em.persist(i);
+		em.getTransaction().commit();
+		em.close();
 	}
 	
 	public void delete(InfoCarrera inf) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
 		em.remove(inf);
+		em.getTransaction().commit();
+		em.close();
 	}
 
 	@Override
 	public void update(InfoCarrera element) {
-
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		if(em.contains(element)){
+			em.merge(element);
+		}else{
+			// si no esta no quiero agregarlo
+		}
+		em.getTransaction().commit();
+		em.close();
 	}
 }
