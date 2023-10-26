4ad271474bc9d39ad7898574ded21f4e20971c04
adangomez8 <adanvgomez1234@gmail.com>
2023-10-26 20:31:33 -0300

    se agrega csv monopatin

--- Microservicios/microservicio-monopatin/src/main/java/com/app/monopatin/MicroservicioMonopatinApplication.java
+++ Microservicios/microservicio-monopatin/src/main/java/com/app/monopatin/MicroservicioMonopatinApplication.java
@@ -1,5 +1,6 @@
 package com.app.monopatin;
 
+import com.app.monopatin.services.MonopatinService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,10 @@ public class MicroservicioMonopatinApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MicroservicioMonopatinApplication.class, args);
+
+        MonopatinService monopatinService = new MonopatinService();
+        CargaDeDatos  cg = new CargaDeDatos(monopatinService);
+        cg.readCSV();
     }
 
 }
